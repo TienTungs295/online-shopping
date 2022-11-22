e7b25af990c9a48851834bd58b6d32cf074f1176
tienlq <tientungs295@gmail.com>
2022-11-22 23:42:39 +0700

    update icon

--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -4,9 +4,8 @@
     <div class="d-flex align-items-center justify-content-between">
         <a href="{!! route('homeView') !!}" class="logo d-flex align-items-center">
             {{--            <img src="assets/img/logo.png" alt="">--}}
-            <span class="d-none d-lg-block">Quản trị</span>
+            <span class="d-none d-lg-block">Quản trị 3MNK</span>
         </a>
-        <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
 
 
--- resources/views/backend/layouts/sidebar_left.blade.php
+++ resources/views/backend/layouts/sidebar_left.blade.php
@@ -9,51 +9,45 @@
             </a>
         </li>
         <li class="nav-item">
+            <a class="nav-link collapsed" href="{!! route("orderView") !!}">
+                <i class="bi bi-cart-check-fill"></i>
+                <span>Đơn hàng</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{!! route("categoryView") !!}">
+                <i class="bi bi-list-ul"></i>
+                <span>Danh mục</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{!! route("collectionView") !!}">
+                <i class="bi bi-collection"></i>
+                <span>Bộ sưu tập</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{!! route("productView") !!}">
+                <i class="bi bi-shop"></i>
+                <span>Sản phẩm</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{!! route("blogView") !!}">
+                <i class="bi bi-file-earmark-text"></i>
+                <span>Bài viết</span>
+            </a>
+        </li>
+        <li class="nav-item">
             <a class="nav-link collapsed" href="{!! route("labelView") !!}">
                 <i class="bi bi-tags"></i>
                 <span>Nhãn</span>
             </a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="{!! route("collectionView") !!}">
-                <i class="bi bi-tags"></i>
-                <span>Bộ sưu tập</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="{!! route("flashSaleView") !!}">
-                <i class="bi bi-tags"></i>
-                <span>Flash Sale</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="{!! route("discountView") !!}">
-                <i class="bi bi-tags"></i>
-                <span>Mã giảm giá</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="{!! route("categoryView") !!}">
-                <i class="bi bi-tags"></i>
-                <span>Danh mục</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="{!! route("blogView") !!}">
-                <i class="bi bi-tags"></i>
-                <span>Bài viết</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="{!! route("productView") !!}">
-                <i class="bi bi-tags"></i>
-                <span>Sản phẩm</span>
-            </a>
-        </li>
         @if((auth()->user()->role & 2) > 0)
             <li class="nav-item">
                 <a class="nav-link collapsed" href="{!! route("userView") !!}">
-                    <i class="bi bi-tags"></i>
+                    <i class="bi bi-person-bounding-box"></i>
                     <span>Nhân viên</span>
                 </a>
             </li>
