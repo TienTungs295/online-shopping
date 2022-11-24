aaec2a98dbf548bf39dde3e87d3744667296a3bf
tienlq <tientungs295@gmail.com>
2022-11-24 22:48:32 +0700

    update contact us

--- resources/js/components/ContactUsComponent.vue
+++ resources/js/components/ContactUsComponent.vue
@@ -68,52 +68,6 @@
               </div>
           </div>
           <!-- END SECTION CONTACT -->
-
-          <!-- START SECTION CONTACT -->
-          <div class="section pt-0">
-              <div class="container">
-                  <div class="row">
-                      <div class="col-lg-6">
-                          <div class="heading_s1">
-                              <h2>Liên lạc</h2>
-                          </div>
-                          <p class="leads">Vui lòng để lại thông tin liên lạc và lời nhắn</p>
-                          <div class="field_form">
-                              <form method="post" name="enq">
-                                  <div class="row">
-                                      <div class="form-group col-md-6">
-                                          <input required placeholder="Họ & tên *" id="first-name" class="form-control" name="name" type="text">
-                                      </div>
-                                      <div class="form-group col-md-6">
-                                          <input required placeholder="Email *" id="email" class="form-control" name="email" type="email">
-                                      </div>
-                                      <div class="form-group col-md-6">
-                                          <input required placeholder="Số điện thoại *" id="phone" class="form-control" name="phone">
-                                      </div>
-                                      <div class="form-group col-md-6">
-                                          <input placeholder="Chủ đề" id="subject" class="form-control" name="subject">
-                                      </div>
-                                      <div class="form-group col-md-12">
-                                          <textarea required placeholder="Nội dung *" id="description" class="form-control" name="message" rows="4"></textarea>
-                                      </div>
-                                      <div class="col-md-12">
-                                          <button type="submit" title="Gửi!" class="btn btn-fill-out" id="submitButton" name="submit" value="Submit">Gửi</button>
-                                      </div>
-                                      <div class="col-md-12">
-                                          <div id="alert-msg" class="alert-msg text-center"></div>
-                                      </div>
-                                  </div>
-                              </form>
-                          </div>
-                      </div>
-<!--                      <div class="col-lg-6 pt-2 pt-lg-0 mt-4 mt-lg-0">-->
-<!--                          <div id="map" class="contact_map2" data-zoom="12" data-latitude="40.680" data-longitude="-73.945" data-icon="assets/images/marker.png"></div>-->
-<!--                      </div>-->
-                  </div>
-              </div>
-          </div>
-          <!-- END SECTION CONTACT -->
-
       </div>
       <!-- END MAIN CONTENT -->
   </div>
