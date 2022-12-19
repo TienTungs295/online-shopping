f0a55c7813f21a6104fb70f015b04527740666dc
tienlq <tientungs295@gmail.com>
2022-12-19 23:14:17 +0700

    update version

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -35,13 +35,13 @@
     <link rel="stylesheet" href="{{ asset('assets/css/slick.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/slick-theme.css') }}">
     <!-- Style CSS -->
-    <link rel="stylesheet" href="{{ asset('css/app.css?version=24') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css?version=25') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
 </head>
 <body>
 <div id="app">
 </div>
-<script src="{{ asset('js/app.js?version=24') }}"></script>
+<script src="{{ asset('js/app.js?version=25') }}"></script>
 <!-- Latest jQuery -->
 <script src="{{asset('assets/js/jquery-3.6.0.min.js')}}"></script>
 <!-- popper min js -->
